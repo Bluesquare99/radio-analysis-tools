d3c2ab9df9c3fedbd34a76912b5dae03a73ee205
Max McCready <msmccready@gmail.com>
2022-02-07 11:17:12 -0600

    improved error handling

--- koop917.py
+++ koop917.py
@@ -5,49 +5,40 @@ import re
 def main():
 
 	schedule = show_schedule.get_schedule()
-	# print(schedule['Monday'])
 
 	def clean_url(show_name):
-		return re.sub('[^0-9a-zA-Z]+','-',show_name.lower().strip())
-
+		show_name_no_apostrophes = show_name.replace("'",'')		
+		show_cleaned = re.sub('[^0-9a-zA-Z]+','-',show_name.replace("'",'').lower().strip())
+		return show_cleaned
 
 	for day in schedule:
 		for k, v in schedule[day].items():
-			if(clean_url(v) == 'nobody-s-happy-hour' 
-				or clean_url(v) == 'austin-artist-collective'
-				or clean_url(v) == 'bringing-light-into-darkness'
-				or clean_url(v) == 'lost-in-paradise-radio'
-				or clean_url(v) == 'the-sex-ed-show'
-				or clean_url(v) == 'the-austin-common-radio-hour'
-				or clean_url(v) == 'civil-rights-and-wrongs'
-				or clean_url(v) == 'off-stage-and-on-the-air'
-				or clean_url(v) == 'texas-music-office-s-texas-music-mixtape'
-				or clean_url(v) == 'crate-digger-s-gold'
-				or clean_url(v) == 'the-stopped-clock'
-				or clean_url(v) == 'texchromosome-radio'
-				or clean_url(v) == 'what-s-new'
-				or clean_url(v) == 'lights-camera-austin'
-				or clean_url(v) == 'pearl-s-general-store'
-				or clean_url(v) == 'people-united'
-				or clean_url(v) == 'rag-radio'):
-				continue
-			print(clean_url(v))
-			print(playlists.get_playlist(clean_url(v)))
+			# if(
+			# 	# clean_url(v) == 'nobody-s-happy-hour' 
+			# 	clean_url(v) == 'austin-artist-collective'
+			# 	or clean_url(v) == 'bringing-light-into-darkness'
+			# 	or clean_url(v) == 'lost-in-paradise-radio'
+			# 	or clean_url(v) == 'the-sex-ed-show'
+			# 	or clean_url(v) == 'the-austin-common-radio-hour'
+			# 	or clean_url(v) == 'civil-rights-and-wrongs'
+			# 	or clean_url(v) == 'off-stage-and-on-the-air'
+			# 	or clean_url(v) == 'texas-music-office-s-texas-music-mixtape'
+			# 	# or clean_url(v) == 'crate-digger-s-gold'
+			# 	or clean_url(v) == 'the-stopped-clock'
+			# 	or clean_url(v) == 'texchromosome-radio'
+			# 	# or clean_url(v) == 'what-s-new'
+			# 	or clean_url(v) == 'lights-camera-austin'
+			# 	# or clean_url(v) == 'pearl-s-general-store'
+			# 	or clean_url(v) == 'people-united'
+			# 	or clean_url(v) == 'rag-radio'):
+			# 	continue
+			try:
+				print(clean_url(v))
+				print(playlists.get_playlist(clean_url(v)))
+			except Exception as e:
+				print(e)
 		
 # print(playlists.get_playlist(clean_url(v)))
 
 if __name__ == "__main__":
 	main()
-
-# XX Create a variable to hold each day and time slot
-# You have the schedule--now you need to find the playlist for each function
-#	This will require another scraping script
-#	This next scraping script will have to dynamically register the urls and their extensions
-#	Clean up the titles of the shows then use those to get the scripts
-#
-#	1. XX Start simple, start with one address
-#		See how the url is composed
-#		It's lower-cased, trimmed, all non-alphanumerics are replaced with -
-#	2. XX Make soup
-#	3. Create a function to get that playlist
-#	4. Separate that function out in another file
--- koop917_playlists.py
+++ koop917_playlists.py
@@ -6,25 +6,33 @@ def get_playlist(show_cleaned):
 	url = 'https://koop.org/programs/' + show_cleaned
 	PATH = '/Users/bluesquarez/Documents/Coding/Resources/chromedriver'
 	driver = webdriver.Chrome(PATH)
-	driver.get(url)
+	try:
+		driver.get(url)
+	except Exception as e:
+		print(e)
 
-	if(driver.find_element_by_class_name("js-programPlaylist") == ''):
-		return
-	playlist_raw = driver.find_element_by_class_name("js-programPlaylist")
-	if(playlist_raw.text == ''):
-		return
-	songs = playlist_raw.text.split('\n')
-	playlist = {}
+	# if(driver.find_element_by_class_name("js-programPlaylist") == ''):
+	# 	return
+	try:
+		playlist_raw = driver.find_element_by_class_name("js-programPlaylist")
+		if(playlist_raw.text == ''):
+			return
+		songs = playlist_raw.text.split('\n')
+		playlist = {}
 
-	for song in songs:
-		song_split = song.split('-')
-		artist = song_split[0].strip()
-		track = song_split[1].split('(')[0].strip()
-		playlist[artist] = track
+		for song in songs:
+			song_split = song.split('-')
+			artist = song_split[0].strip()
+			track = song_split[1].split('(')[0].strip()
+			playlist[artist] = track
+		
+		return(playlist)
+	except Exception as e:
+		print(e)
+	finally:
+		driver.quit()
 
-	driver.quit()
-
-	return(playlist)
+	
 
 if __name__ == "__main__":
-	print('Get playlists is being called directly')
+	print(get_playlist('nobodys-happy-hour'))
